ce4728ba5f1205e8d0998a8b044323687486e31f
Shree Ram <mekashreeram@gmail.com>
2024-02-05 21:54:35 +0530

    remove unneccessary lines in twoa.py

--- twoa.py
+++ twoa.py
@@ -7,12 +7,11 @@ def ceasar_dec(cipher):
   for key in range((len(ALPHABET))):
     translated = ''
     for symbol in cipher:
-      if symbol in cipher:
-        ind = ALPHABET.find(symbol)
-        ind = ind - key
-        if ind < 0:
-          ind += len(ALPHABET)
-        translated += ALPHABET[ind]
+      ind = ALPHABET.index(symbol)
+      ind = ind - key
+      if ind < 0:
+        ind += len(ALPHABET)
+      translated += ALPHABET[ind]
       
     print(f'key #{key}: {translated}')
 
