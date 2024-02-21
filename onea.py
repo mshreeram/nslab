3003db6b3ba96d9f280f3965c19ef5c2eabf0433
JayaDeep <110987107+JayaDeep11@users.noreply.github.com>
2024-02-21 09:37:16 +0530

    update code...
    we cannot change the data given by user.

--- onea.py
+++ onea.py
@@ -5,9 +5,15 @@ s = int(input("Enter a shift value: "))
 
 def ceasar_enc(text, s):
   result = ""
-  text = text.lower()
-  for char in text:
-    result += chr((ord(char) + s - 97) % 26 + 97)
-  print(result)
+# we cannot change the data given by the user.
 
+for char in text:
+    if char.isalpha():  
+        if char.islower():
+            result += chr((ord(char) - 97 + s) % 26 + 97)
+        else:
+            result += chr((ord(char) - 65 + s) % 26 + 65)
+    else:
+        result += char  
+    return result
 ceasar_enc(text, s)
